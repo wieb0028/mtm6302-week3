2c4cb32a4bf8422c63b2f87d86239b2200deabb4
wieb0028 <wieb0028@algonquinlive.com>
2023-05-26 13:57:17 -0400

    more javascript practice

--- js/main.js
+++ js/main.js
@@ -16,3 +16,35 @@ console.log(container)
 // using style objects of an element
 container.style.maxWidth ="800px"
 container.style.margin = "0 auto"
+
+// finding all the elements using css selector
+let allNodes = document.querySelectorAll(".type")
+console.log(allNodes)
+
+// accessing the element on a second postition and setting its style background
+allNodes[1].style.background = "cyan"
+
+// looping through the allNodes array and assigning style.border to each item
+for(let eachNode of allNodes){
+    eachNode.style.border = "1px solid black"
+}
+
+// traversing the DOM
+
+// finding element by id
+const mainNodes = document.getElementById("threeNodes")
+console.log(mainNodes)
+
+// accessing the children property of the mainNotes variable
+const listOfNodes =mainNodes.children
+
+// selecting a second item and getting its styles
+listOfNodes[1].style.margin = "10px 0"
+
+const firstNode = mainNodes.firstElementChild
+console.log(firstNode.textContent)
+
+// attribute methodes
+const learnMore = document.getElementById("learn")
+
+console.log(learnMore.getAttribute("href"))
